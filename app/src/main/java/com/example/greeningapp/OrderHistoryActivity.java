02c3ad7fc8b6803362d0768423721f3f63cf4931
JY <wxv4869@gmail.com>
2023-07-20 01:11:44 +0900

    박윤주 기능 추가

---
+++ app/src/main/java/com/example/greeningapp/OrderHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.greeningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_history);
+    }
+}
